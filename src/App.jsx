bd27d655880b93ff870c5bd41ac2744a607d7a8f
Robin <harmonieyacht@gmail.com>
2026-05-04 15:08:30 +0000

    feat: nouveaux tarifs - Prestige 380€, 2h 400€, 3h 600€, 4h 800€

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const LAT=43.5283,LON=3.9816;
 const MN=["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
 const MNC=["Jan","Fév","Mar","Avr","Mai","Jui","Jul","Aoû","Sep","Oct","Nov","Déc"];
 const DL=["L","M","M","J","V","S","D"],DFLS=["Lun","Mar","Mer","Jeu","Ven","Sam","Dim"];
-const TX={nuit_classique:180,nuit_prestige:350,nuit_signature:750,sortie_mer_2h:380,sortie_mer_3h:550,sortie_mer_4h:750};
+const TX={nuit_classique:180,nuit_prestige:380,nuit_signature:750,sortie_mer_2h:400,sortie_mer_3h:600,sortie_mer_4h:800};
 const FM={nuit_classique:{c:"#007AFF",l:"Essentielle",i:"🌙"},nuit_prestige:{c:"#AF52DE",l:"Prestige",i:"✨"},nuit_signature:{c:"#C9A96E",l:"Signature",i:"👑"},sortie_mer_2h:{c:"#30D158",l:"Sortie 2h",i:"⛵"},sortie_mer_3h:{c:"#34C759",l:"Sortie 3h",i:"⛵"},sortie_mer_4h:{c:"#28BD4F",l:"Sortie 4h",i:"⛵"}};
 const SOURCES=[
   {v:"instagram_dm",l:"Instagram DM"},
@@ -16,7 +16,7 @@ const SOURCES=[
   {v:"manuel",l:"Manuel"},
   {v:"autre",l:"Autre"},
 ];
-const TYPES=[{v:"nuit_classique",l:"Essentielle 180€"},{v:"nuit_prestige",l:"Prestige 350€"},{v:"nuit_signature",l:"Signature 750€"},{v:"sortie_mer_2h",l:"Sortie 2h 380€"},{v:"sortie_mer_3h",l:"Sortie 3h 550€"},{v:"sortie_mer_4h",l:"Sortie 4h 750€"}];
+const TYPES=[{v:"nuit_classique",l:"Essentielle 180€"},{v:"nuit_prestige",l:"Prestige 380€"},{v:"nuit_signature",l:"Signature 750€"},{v:"sortie_mer_2h",l:"Sortie 2h 400€"},{v:"sortie_mer_3h",l:"Sortie 3h 600€"},{v:"sortie_mer_4h",l:"Sortie 4h 800€"}];
 const STATUTS=[{v:"nouveau",l:"Nouveau"},{v:"en_conversation",l:"En conversation"},{v:"qualifie",l:"Qualifié"},{v:"reserve",l:"Réservé"},{v:"termine",l:"Terminé"},{v:"perdu",l:"Perdu"}];
 const TEMPS=[{v:"chaud",l:"Chaud"},{v:"tiede",l:"Tiède"},{v:"froid",l:"Froid"}];
 const DT={nuit_classique:[17,12],nuit_prestige:[17,12],nuit_signature:[17,12],sortie_mer_2h:[10,12],sortie_mer_3h:[10,13],sortie_mer_4h:[10,14]};
